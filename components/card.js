94167d59ee3d45097a15bbf44bf4ffe09b15e083
udayatulasi <binnuuday05@gmail.com>
2021-02-19 12:18:00 -0400

    css added

--- components/card.js
+++ components/card.js
@@ -1,4 +1,4 @@
-// import { makeStyles, StylesProvider } from '@material-ui/core/styles';
+import { makeStyles, StylesProvider } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
@@ -8,43 +8,43 @@ import { Grid } from '@material-ui/core';
 import Link from "next/link"
 
 
-// const useStyles = makeStyles({
-//   root: {
-//     minWidth: 275,
-//   },
-//   bullet: {
-//     display: 'inline-block',
-//     margin: '0 2px',
-//     transform: 'scale(0.8)',
-//   },
-//   title: {
-//     fontSize: 20,
-//     display: "center",
-//     textAlign:"center"
-//   },
-//   pos: {
-//     marginBottom: 12,
-//   },
-//   media: {
-//     height: 0,
-//     padding: '20%', // 16:9
-//   },
-// });
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 20,
+    display: "center",
+    textAlign:"center"
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  media: {
+    height: 0,
+    padding: '20%', // 16:9
+  },
+});
 
 export default function countryCard(props) {
-//   const classes = useStyles();
+  const classes = useStyles();
 
   return (
       <Link href="/[country]" as={`/${props.country.name}`}>
       <Grid item xs={6} md={4}>
-    <Card  >
+    <Card className={classes.root} >
          <CardMedia
-        
+        className={classes.media}
         image={props.country.flag}
         title="Paella dish"
       />
       <CardContent>
-        <Typography  color="textSecondary" gutterBottom>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
           {props.country.name}
         </Typography>
         </CardContent>
